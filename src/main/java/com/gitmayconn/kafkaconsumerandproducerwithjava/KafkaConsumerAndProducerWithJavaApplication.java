6c8043477bd03ba73469c7d1bd7898e3157bb14e
= <3rd.mayconn.lbispo@americanas.io>
2023-05-27 17:29:56 -0300

    Adiciona anotação para habilitar o kafka no projeto

--- src/main/java/com/gitmayconn/kafkaconsumerandproducerwithjava/KafkaConsumerAndProducerWithJavaApplication.java
+++ src/main/java/com/gitmayconn/kafkaconsumerandproducerwithjava/KafkaConsumerAndProducerWithJavaApplication.java
@@ -2,10 +2,13 @@ package com.gitmayconn.kafkaconsumerandproducerwithjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class KafkaConsumerAndProducerWithJavaApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerAndProducerWithJavaApplication.class, args);
 	}
