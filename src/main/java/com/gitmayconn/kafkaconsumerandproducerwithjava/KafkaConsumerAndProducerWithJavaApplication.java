b132f3a83c19498434ca3ae9517b19a03638f217
= <3rd.mayconn.lbispo@americanas.io>
2023-05-27 14:02:49 -0300

    commit inicial

---
+++ src/main/java/com/gitmayconn/kafkaconsumerandproducerwithjava/KafkaConsumerAndProducerWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gitmayconn.kafkaconsumerandproducerwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumerAndProducerWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumerAndProducerWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitmayconn/kafkaconsumerandproducerwithjava/KafkaConsumerAndProducerWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitmayconn.kafkaconsumerandproducerwithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerAndProducerWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
